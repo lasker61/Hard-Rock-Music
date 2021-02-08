1f4ae9d5b0e8cda86b9953188f3647ea6076d820
lasker61 <laskernahid2@gmail.com>
2021-02-08 21:22:13 +0600

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const searchSongs = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url =` https://api.lyrics.ovh/suggest/${searchText}`
+    fetch(url)
+    .then(response =>response.json())
+    .then(data => displaySongs(data.data));
+}
+
+const displaySongs = songs =>{
+    songs.forEach(song => console.log(song.title));
+
+}
